b124f5df2e2bd441c1bc686143fc88f6224afc94
Rayane Ventura <rayyventura@gmail.com>
2026-09-25 00:32:10 +0100

    Let GitHub Actions assume the deploy role and build the images.
    The role trust now matches the repository subject GitHub sends, and the service images include the shared TypeScript config.

--- infra/envs/dev/main.tf
+++ infra/envs/dev/main.tf
@@ -407,7 +407,10 @@ module "cdn" {
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c58a3a8518e8759bf075b76b750d4f2df264fcd",
+  ]
 }
 
 resource "aws_iam_role" "github_deploy" {
@@ -426,7 +429,7 @@ resource "aws_iam_role" "github_deploy" {
           "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
         }
         StringLike = {
-          "token.actions.githubusercontent.com:sub" = "repo:rayyventura/docpost:*"
+          "token.actions.githubusercontent.com:sub" = "repo:rayyventura*/docpost*:*"
         }
       }
     }]
